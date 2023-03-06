c4d7e14e0826111f7a6ce537e079ee48c9d40868
sapunethmini <122308654+sapunethmini@users.noreply.github.com>
2023-03-06 13:28:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,175 @@
+
+import './App.css';
+
+
+    function App() {
+      return (
+        <><table class="d1">
+          <tr>
+            <td>
+              <p class="s1">Sapuni</p>
+            </td>
+            <td>
+              <div class="s2">
+                <pre>
+                  Home  About  Experience  portfolio  Blog  Contact
+                </pre>
+              </div>
+            </td>
+          </tr>
+        </table>
+        <img src="001.jpeg" class="img1" align="right" /><br /><br /><br /><br /><br /><br /><table class="d2">
+            <tr>
+              <td class="s3">Hello</td>
+            </tr>
+            <tr>
+              <td class="s5">i'm</td>
+              <td class="s4">Sapuni nethmini</td>
+
+            </tr>
+          </table><table class="d3">
+            <tr>
+              <td><kbd>Ul/UX Designer & Developer | Freelancer </kbd></td>
+            </tr>
+          </table><table class="d4">
+            <tr>
+              <td>I’m a designer with extensive experience for over 3 years.<br />
+                My expertise is to create and design website at mockups and many more..</td>
+            </tr>
+          </table><table class="d5">
+            <tr>
+              <td class="buttons">
+                <a href="#vsn">
+                  <button class="button">View Portfolio</button>
+                </a></td>
+            </tr>
+          </table><br /><br /><br /><br /><br /><br /><br /><hr class="new5" /><br /><br /><br /><table class="d7">
+            <tr>
+              <td>
+
+                <figure>
+
+                  <img src="002.jpeg" class="img2" width="200" height="400"></img>
+                  <figcaption>
+                    <table class="d6">
+                      <tr>
+                        <td class="c9">Designer</td>
+                      </tr>
+                      <tr>
+                        <td class="s11" width="100px">Sapuni Nethmini</td>
+                      </tr>
+                      <tr>
+                        <th align="left"><img src="003.png" class="img3" /></th>
+                      </tr>
+
+                    </table>
+
+                  </figcaption>
+                </figure>
+
+              </td>
+              <td>
+                <table align="left" class="d8">
+                  <tr>
+
+                    <td class="s8" id="vsn"> Let me introduce my self</td>
+                  </tr>
+                  <tr>
+                    <td class="s9">About me</td>
+                  </tr>
+                  <tr>
+                    <td class="a"> I have a strong passion for design. I specialized in UI/ UK design and my
+                      passion is all about building elegant and professional user interface and
+                      websites.<br />
+                      I also do branding and Identity Design such as logo design. letterhead and
+                      business card alone with photo editing image cropping and other graphic
+                      design service. </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <table class="d9">
+                        <tr>
+                          <td class="s22">Name   :</td>
+                          <td class="b">Sapuni Nethmini</td>
+                        </tr>
+                        <tr>
+                          <td class="s22">Birthday   :</td>
+                          <td class="b">21st january 2000</td>
+                        </tr>
+                        <tr>
+                          <td class="s22">Age   :</td>
+                          <td class="b"> 22 y/o</td>
+                        </tr>
+                        <tr>
+                          <td class="s22">Address   :</td>
+                          <td class="b"> Hambanthota, Sri Lanka</td>
+                        </tr>
+                        <tr>
+                          <td class="s22">University   :</td>
+                          <td class="b"> <a href="https://ucsc.cmb.ac.lk/">
+                            university of colombo</a></td>
+                        </tr>
+                        <tr>
+                          <td class="s22">Email   :</td>
+                          <td class="b">sapuninethmini888@gmail.com</td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table><br /><br /><br /><br /><br /><br /><br /><hr class="new5" /><br /><br /><br />
+            <table>
+              <tr>
+                <td class="d67">Education</td>
+              </tr>
+            </table><br /><br /><br />
+
+            <table class="d68">
+              <tr>
+                <td class="s14">Bachelors's Degree<br />
+
+                  University of Colombo<br /></td>
+              </tr>
+
+
+              2022 - Present<br />
+
+              Full Time
+
+              <td class="s14">
+                Bsc. (Hons) in Information Systems
+              </td><tr>
+                <td class="s14">School education<br />
+
+                  Weeraketiya central collage weerketiya <br />
+
+                  2006-2019<br />
+
+                  Full Time</td>
+                <td class="s14">  Primary and Secondary Education<br />
+                  passed o.l examination with A9 and also passed a.l <br />                           examination with 2B and
+                  1C</td>
+              </tr>
+            </table>
+            <hr class="new5" /><br /><br /><br />
+            <table>
+              <tr>
+                <td class="d67">Awards</td>
+              </tr>
+            </table>
+            <br /><br /><br />
+
+            <table class="g99">
+              <tr>
+                <td><img src="004.png" class="img4" /></td>
+                <td> <img src="005.png" class="img4" /></td>
+                <td><img src="006.png" class="img4" /></td>
+              </tr>
+            </table>
+            </>
+            );
+          }
+
+ export default App;
